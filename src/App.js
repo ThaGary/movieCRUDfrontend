97e8fa5b7469b5e2a4edd4864d681ce95926d35b
ThaGary <garyanderson31591@gmail.com>
2018-12-06 16:04:23 -0700

    build and displaying movie when clicked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './movies.js';
-
 class App extends Component {
   constructor() {
     super()
     this.state = { 
-      movies : null 
+      movies : null,
+      info: null 
     }
   }
   async componentDidMount() {
@@ -14,11 +14,29 @@ class App extends Component {
     .then(response => response.json())
     .then(movies => this.setState({ movies }))
   }
+  handleClick = (event) => {
+    fetch('https://garysmovies.herokuapp.com/'+event.target.id)
+    .then(response => response.json())
+    .then(info => this.setState({ info }))
+    console.log(event.target.id)
+  }
     render() {
+      console.log(this.state.info)
         return ( 
           <div className="movies" >
-            {this.state.movies ? <Movies movies={this.state.movies}/> : <div>Loading</div>}
+            {this.state.info && 
+            <div className="media">
+            <img className="mr-3" src={this.state.info.poster} alt="" width='100px' />
+            <div className="media-body">
+              <h5 className="mt-0">{this.state.info.title}</h5>
+              <h5>Director: {this.state.info.director}</h5>
+              <h5>Year: {this.state.info.year}</h5>
+              <h5>Score: {this.state.info.rating}</h5>
+            </div>
+          </div>}
+            {this.state.movies ? <Movies handleClick={this.handleClick} movies={this.state.movies}/> : <div>Loading</div>}
           </div>
+
         );
     }
   }
--- src/movies.js
+++ src/movies.js
@@ -4,15 +4,12 @@ const Movies = (props) => {
     return (
         <div className="movies">
             {props.movies.map(item => {
-                const handleClick = () => {
-                    console.log(item.id, item.title, item.director, item.year, item.rating);
-                  }
                 return (
                     <div>
                         <table className="table">
                             <tbody>
                                 <tr>
-                                    <td onClick={handleClick}>{item.title}</td>
+                                    <td id={item.id} onClick={props.handleClick}>{item.title}</td>
                                 </tr>
                             </tbody>
                         </table>
